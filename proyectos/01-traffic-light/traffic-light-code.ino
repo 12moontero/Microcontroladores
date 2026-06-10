0c7dcbd1adc4574e394f7670e16b65db8528cb0f
David <davidlimamente@gmail.com>
2026-06-10 11:13:36 +0200

    Update traffic-light-code.ino

--- proyectos/01-traffic-light/traffic-light-code.ino
+++ proyectos/01-traffic-light/traffic-light-code.ino
@@ -1,1 +1,30 @@
+// C++ code
+int ledGreen = 13;
+int ledOrange = 12;
+int ledRed = 11;
 
+void setup()
+{
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledOrange,OUTPUT);
+  pinMode(ledRed,OUTPUT);
+}
+
+void loop()
+{
+  
+// Led Green
+digitalWrite(ledGreen,HIGH);
+delay(6000);
+digitalWrite(ledGreen,LOW);
+
+// Led Orange
+digitalWrite(ledOrange,HIGH);
+delay(6000);
+digitalWrite(ledOrange,LOW);
+
+// led Red
+digitalWrite(ledRed,HIGH);
+delay(6000);
+digitalWrite(ledRed,LOW);
+}
