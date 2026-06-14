e1928902adfdb3417bf77d468cfb8193503f6225
David <davidlimamente@gmail.com>
2026-06-14 12:41:49 +0200

    Update v1.0-police-lights-code.ino

--- proyectos/02-police-lights/v1.0/v1.0-police-lights-code.ino
+++ proyectos/02-police-lights/v1.0/v1.0-police-lights-code.ino
@@ -1,1 +1,22 @@
+//C++
+int ledRed = D7;
+int ledBlue = D6;
 
+void setup()
+{  
+  pinMode(ledRed,OUTPUT);
+  pinMode(ledBlue, OUTPUT);
+}
+
+void loop()
+{
+// Led Red
+digitalWrite(ledRed,HIGH);
+delay(100);
+digitalWrite(ledRed,LOW);
+
+// Led Blue
+digitalWrite(ledBlue,HIGH);
+delay(100);
+digitalWrite(ledBlue,LOW);
+}
